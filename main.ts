2356a25fe8956127fb5e6ecf5b1ee8696975e5e9
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-09-04 19:37:37 +0500

    Code : Array for tasks &  addTask func

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
 const taskInput = document.getElementById("taskInput") as HTMLInputElement;
 const addBtn = document.getElementById("addBtn") as HTMLButtonElement;
 const taskList = document.getElementById("taskList") as HTMLUListElement;
+
+
+let tasks: string[] = [];
+
+function addTask() {
+  const task = taskInput.value.trim();
+
+  if (task) {
+    tasks.push(task);
+    renderTasks();
+    taskInput.value = "";
+  }
+}
