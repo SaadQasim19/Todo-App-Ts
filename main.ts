f8035ea270e4eb040134d53f8ed1ffd9923f8f31
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-09-04 19:35:28 +0500

    Code : selected dom elements

---
+++ main.ts
@@ -0,0 +1,3 @@
+const taskInput = document.getElementById("taskInput") as HTMLInputElement;
+const addBtn = document.getElementById("addBtn") as HTMLButtonElement;
+const taskList = document.getElementById("taskList") as HTMLUListElement;
